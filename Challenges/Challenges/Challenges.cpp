b9ec1aeaff4738b40392c9029ef4b5d2c28d4080
Evan <evanfretheim@gmail.com>
2021-09-24 18:23:32 -0500

    current

---
+++ Challenges/Challenges/Challenges.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    int num = 5;
+    double dubnum = 5.0;
+    char letter = 'E';
+    bool boolean = true;
+    string text = "Hello World! ";
+    string text2 = "My name is Evan \n";
+    const string myName = "Evan Fretheim";
+    int x;
+    float myfloat = 5.55;
+    string full = text + text2;
+    string howlong = "Hello, I am wondering how my characters are in this sentence.";
+    int num1 = 5;
+    int num2 = 10;*/
+
+    /* this is all a comment kadsljfasffffsfsfsfsfsfsfsfsfsfsfsfsfsfsf
+    asdflksadlkfdskljfkljsadfkljsadlkfaslkdjfjkl 
+    more comment ksdlfkasdklfklajsd*/
+
+
+    /*cout << "Whats your favorite number?\n";
+    cin >> x;
+    cout << "Oh cool! Your favorite number is: " << x; " My favorite number is 15!\n";
+
+    cout << "\nHello World!\n";
+    cout << "New Line\n";
+    cout << full;
+    cout << howlong.length();
+    cout << "The 3rd character in the sentence is :" << howlong[2];
+
+
+    cout << (num1 > num2);
+    cout << (num1 == num2);
+    cout << (num1 != num2);
+    cout << (num1 <= num2);*/
+
+
+    /*string fullname;
+    cout << "What is your name? \n";
+    getline(cin, fullname);
+    cout << "Your name is: " << fullname << endl;
+
+    cout << max(5, 10) << endl;
+
+    cout << sqrt(64) << endl;
+
+    cout << round(2.235) << endl;
+
+    cout << pow(2, 2) << endl;*/
+}
